730f2c0a57ded2635624d44d51ce43a55f63a311
crabdancing <alxpettit@gmail.com>
2024-09-13 12:02:36 -0600

    plasticity dpi control

--- flake.nix
+++ flake.nix
@@ -15,20 +15,6 @@
       pkgs = import "${nixpkgs}" {
         system = "x86_64-linux";
       };
-      # note: upstream's wine versions of mono and gecko are not available in nixpkgs's exposed package collection
-      # and are handled as internal derivations, as seen here:
-      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/applications/emulators/wine/base.nix#L196
-      # thus, we are copying them directly into our flake.
-      # gecko32 = pkgs.fetchurl rec {
-      #   version = "2.47.4";
-      #   url = "https://dl.winehq.org/wine/wine-gecko/${version}/wine-gecko-${version}-x86.msi";
-      #   hash = "sha256-Js7MR3BrCRkI9/gUvdsHTGG+uAYzGOnvxaf3iYV3k9Y=";
-      # };
-      # gecko64 = pkgs.fetchurl rec {
-      #   version = "2.47.4";
-      #   url = "https://dl.winehq.org/wine/wine-gecko/${version}/wine-gecko-${version}-x86_64.msi";
-      #   hash = "sha256-5ZC32YijLWqkzx2Ko6o9M3Zv3Uz0yJwtzCCV7LKNBm8=";
-      # };
 
       # I have tried unstable, and wine-ge can not seem to find it :(
       # sources = (import "${self.inputs.nixpkgs}/pkgs/applications/emulators/wine/sources.nix" {inherit pkgs;}).unstable;
@@ -44,17 +30,23 @@
         ];
         # build = "full";
       };
-    in {
-      default = self.packages.x86_64-linux.plasticity;
-
-      inherit wine;
-      plasticity = pkgs.callPackage ./plasticity.nix {
+      basePlasticity = pkgs.callPackage ./plasticity.nix {
         inherit self;
         inherit (erosanix.lib.x86_64-linux) mkWindowsApp makeDesktopIcon copyDesktopIcons;
         inherit wine;
-
-        # wine = wineWowPackages.full;
       };
+    in {
+      plasticityBigDPI = basePlasticity.override {
+        setDPI = 90;
+      };
+      plasticity = basePlasticity.override {
+        setDPI = 60;
+      };
+      default = self.packages.x86_64-linux.plasticity;
+
+      inherit basePlasticity;
+
+      inherit wine;
     };
 
     apps.x86_64-linux.plasticity = {
--- plasticity.nix
+++ plasticity.nix
@@ -11,185 +11,102 @@
   unzip,
   system,
   self,
+  pkgs,
+  setDPI ? null,
 }: let
-  # The default settings used if user doesn't already have a settings file.
-  # Tabs are disabled because they lead to UI issues when using Wine.
-  # defaultSettings = ./SumatraPDF-settings.txt;
   # This registry file sets winebrowser (xdg-open) as the default handler for
   # text files, instead of Wine's notepad.
-  # Selecting "Settings -> Advanced Options" should then use xdg-open to open the SumatraPDF config file.
+  pname = "plasticity";
   txtReg = ./txt.reg;
+  # Contains GPU cache, code cache, window state,
+  # and the machine's unique license sig
+  stateDir = "$HOME/.local/share/${pname}/roaming";
+
+  setDPIReg = pkgs.writeText "set-dpi-${toString setDPI}.reg" ''
+    Windows Registry Editor Version 5.00
+    [HKEY_LOCAL_MACHINE\System\CurrentControlSet\Hardware Profiles\Current\Software\Fonts]
+    "LogPixels"=dword:${toString setDPI}
+  '';
 in
   mkWindowsApp rec {
-    inherit wine;
-    # wine = self.inputs.nix-gaming.packages.${system}.wine-ge;
+    inherit wine pname;
 
-    pname = "plasticity";
     version = "24.2.2";
 
     src = builtins.fetchurl {
       url = "https://github.com/nkallen/plasticity/releases/download/v${version}/Plasticity.msi";
-      # sha256 = lib.fakeHash;
       sha256 = "sha256:1llqb3w8dwd0kdf96d5vkz7zgz05hdyjhp7i84s4pzm8zfnvhssq";
     };
 
-    # In most cases, you'll either be using an .exe or .zip as the src.
-    # Even in the case of a .zip, you probably want to unpack with the launcher script.
     dontUnpack = true;
-
-    # You need to set the WINEARCH, which can be either "win32" or "win64".
-    # Note that the wine package you choose must be compatible with the Wine architecture.
     wineArch = "win64";
 
-    # Sometimes it can take a while to install an application to generate an app layer.
-    # `enableInstallNotification`, which is set to true by default, uses notify-send
-    # to generate a system notification so that the user is aware that something is happening.
-    # There are two notifications: one before the app installation and one after.
-    # The notification will attempt to use the app's icon, if it can find it. And will fallback
-    # to hard-coded icons if needed.
-    # If an app installs quickly, these notifications can actually be distracting.
-    # In such a case, it's better to set this option to false.
-    # This package doesn't benefit from the notifications, but I've explicitly enabled them
-    # for demonstration purposes.
     enableInstallNotification = true;
-
-    # `fileMap` can be used to set up automatic symlinks to files which need to be persisted.
-    # The attribute name is the source path and the value is the path within the $WINEPREFIX.
-    # But note that you must ommit $WINEPREFIX from the path.
-    # To figure out what needs to be persisted, take at look at $(dirname $WINEPREFIX)/upper,
-    # while the app is running.
     fileMap = {
-      # "$HOME/.cache/${pname}" = "drive_c/${pname}/${pname}cache";
-      # "$HOME/Desktop" = "drive_c/users/$USER";
-      "$HOME/.local/share/mkWindowsApp/${pname}/AppData" = "drive_c/users/$USER/AppData";
-      # "$HOME/Documents" = "drive_c/users/$USER/Documents";
+      "${stateDir}" = "drive_c/users/$USER/AppData/Roaming/Plasticity";
     };
 
-    # By default, `fileMap` is applied right before running the app and is cleaned up after the app terminates. If the following option is set to "true", then `fileMap` is also applied prior to `winAppInstall`. This is set to "false" by default.
     fileMapDuringAppInstall = false;
-
-    # By default `mkWindowsApp` doesn't persist registry changes made during runtime. Therefore, if an app uses the registry then set this to "true". The registry files are saved to `$HOME/.local/share/mkWindowsApp/$pname/`.
     persistRegistry = false;
-    # persistRegistry = true;
-
-    # By default mkWindowsApp creates ephemeral (temporary) WINEPREFIX(es).
-    # Setting persistRuntimeLayer to true causes mkWindowsApp to retain the WINEPREFIX, for the short term.
-    # This option is designed for apps which can't have their automatic updates disabled.
-    # It allows package maintainers to not have to constantly update their mkWindowsApp packages.
-    # It is NOT meant for long-term persistance; If the Windows or App layers change, the Runtime layer will be discarded.
     persistRuntimeLayer = false;
-
-    # The method used to calculate the input hashes for the layers.
-    # This should be set to "store-path", which is the strictest and most reproduceable method. But it results in many rebuilds of the layers since the slightest change to the package inputs will change the input hashes.
-    # An alternative is "version" which is a relaxed method and results in fewer rebuilds but is less reproduceable. If you are considering using "version", contact me first. There may be a better way.
     inputHashMethod = "store-path";
 
     nativeBuildInputs = [unzip copyDesktopItems copyDesktopIcons];
 
-    # This code will become part of the launcher script.
-    # It will execute if the application needs to be installed,
-    # which would happen either if the needed app layer doesn't exist,
-    # or for some reason the needed Windows layer is missing, which would
-    # invalidate the app layer.
-    # WINEPREFIX, WINEARCH, AND WINEDLLOVERRIDES are set
-    # and wine, winetricks, and cabextract are in the environment.
-    # d="$WINEPREFIX/drive_c/${pname}"
-    # config_dir="$HOME/.config/plasticity"
-
     # https://www.advancedinstaller.com/silent-install-exe-msi-applications.html
-    winAppInstall = ''
-      $WINE msiexec /i ${src} /qb!
-      regedit ${txtReg}
-    '';
-    # cp -v -n "${defaultSettings}" "$config_dir/SumatraPDF-settings.txt"
-    # chmod ug+w "$config_dir/SumatraPDF-settings.txt"
-
-    # This code runs before winAppRun, but only for the first instance.
-    # Therefore, if the app is already running, winAppRun will not execute.
-    # Use this to do any setup prior to running the app.
+    winAppInstall =
+      ''
+        $WINE msiexec /i ${src} /qb!
+        regedit ${txtReg}
+      ''
+      + lib.optionalString (setDPI != null) ''
+        regedit ${setDPIReg}
+      '';
     winAppPreRun = ''
     '';
 
-    # This code will become part of the launcher script.
-    # It will execute after winAppInstall and winAppPreRun (if needed),
-    # to run the application.
-    # WINEPREFIX, WINEARCH, AND WINEDLLOVERRIDES are set
-    # and wine, winetricks, and cabextract are in the environment.
-    # Command line arguments are in $ARGS, not $@
-    # DO NOT BLOCK. For example, don't run: wineserver -w
     winAppRun = ''
       wine "$WINEPREFIX/drive_c/Program Files/Plasticity/Plasticity.exe" "$ARGS"
     '';
 
-    # This code will run after winAppRun, but only for the first instance.
-    # Therefore, if the app was already running, winAppPostRun will not execute.
-    # In other words, winAppPostRun is only executed if winAppPreRun is executed.
-    # Use this to do any cleanup after the app has terminated
     winAppPostRun = "";
 
-    # This is a normal mkDerivation installPhase, with some caveats.
-    # The launcher script will be installed at $out/bin/.launcher
-    # DO NOT DELETE OR RENAME the launcher. Instead, link to it as shown.
     installPhase = ''
       runHook preInstall
-
       ln -s $out/bin/.launcher $out/bin/${pname}
-
       runHook postInstall
     '';
 
     desktopItems = let
-      # mimeTypes = [
-      #   "application/pdf"
-      #   "application/epub+zip"
-      #   "application/x-mobipocket-ebook"
-      #   "application/vnd.amazon.mobi8-ebook"
-      #   "application/x-zip-compressed-fb2"
-      #   "application/x-cbt"
-      #   "application/x-cb7"
-      #   "application/x-7z-compressed"
-      #   "application/vnd.rar"
-      #   "application/x-tar"
-      #   "application/zip"
-      #   "image/vnd.djvu"
-      #   "image/vnd.djvu+multipage"
-      #   "application/vnd.ms-xpsdocument"
-      #   "application/oxps"
-      #   "image/jpeg"
-      #   "image/png"
-      #   "image/gif"
-      #   "image/webp"
-      #   "image/tiff"
-      #   "image/tiff-multipage"
-      #   "image/x-tga"
-      #   "image/bmp"
-      #   "image/x-dib"
-      # ];
+      mimeTypes = [
+        "application/x-Plasticity"
+        "application/x-plasticity"
+      ];
     in [
       (makeDesktopItem {
-        # inherit mimeTypes;
+        inherit mimeTypes;
 
         name = pname;
         exec = pname;
         icon = pname;
-        desktopName = "Windows Plasticity";
-        genericName = "3D CAD software for artists, I guess.";
+        desktopName = "Plasticity for Windows";
+        genericName = "3D CAD software for Windows-using artists, I guess.";
         categories = ["Graphics" "Viewer"];
       })
     ];
 
-    # desktopIcon = makeDesktopIcon {
-    #   name = "plasticity";
+    desktopIcon = makeDesktopIcon {
+      name = "plasticity";
 
-    #   src = fetchurl {
-    #     url = "https://plasticity.com/images/moi001.png";
-    #     sha256 = "sha256-c+B847cKvtp5ZUvpoJ7JvgKRH95gdTngS6jyBxkXBvA=";
-    #   };
-    # };
+      src = fetchurl {
+        url = "https://www.plasticity.xyz/_next/image?w=256&q=75&url=%2F_next%2Fstatic%2Fmedia%2Ficon_256x256.09a58ec3.png";
+        sha256 = "sha256-OAmFMeIsrMogwTYiney7rNcKkjbSj/64kGb+6zdbRtA=";
+      };
+    };
 
     meta = with lib; {
-      description = "Windows Plasticity";
-      homepage = "https://vroid.com/en/studio";
+      description = "Plasticity (Proton version)";
+      homepage = "https://www.plasticity.xyz/";
       license = licenses.unfree;
       maintainers = with maintainers; [];
       platforms = ["x86_64-linux"];
